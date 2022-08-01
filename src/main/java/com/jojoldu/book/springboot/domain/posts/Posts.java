76e006f1fe92fc58bf9d641b96aac1f48167e10e
nikeisme <99165573+nikeisme@users.noreply.github.com>
2022-08-01 23:52:40 +0900

    웹 서비스 구현하기

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.domain.posts;
 
+import com.jojoldu.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Generated;
 import lombok.Getter;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor // 기본 생성자 자동 추가
 @Entity // 테이블과 링크될 클래스임을 나타내는 어노테이션, 기본값으로 클래스의 카멜케이스 이름을 _로 테이블 이름을 매칭
-public class Posts{
+public class Posts extends BaseTimeEntity {
 
     @Id // 해당 테이블의 PK 필드를 나타냄
     @GeneratedValue(strategy = GenerationType.IDENTITY)
